baefc0394547f76938cf953cd9a8d3a0d6b480f1
kming <2459753660@qq.com>
2026-07-10 17:48:14 +0800

    Router 性能优化 + 寰球救援免费按钮
    - Router 优化：移除 executePath 恢复循环（省 ~2.4s/跳），
      performBack 返回页面对象避免重复 detectCurrentPage，
      verifyAfterChange 一次确认而非 5 次轮询，移除多余 sleep(500)
    - 寰球救援 免费() 支持 3 次重试+关弹窗+点 2 次
    - 注册 异域挑战、每日一刀 等页面到 Router 注册表
    - 更新路由设计文档流程图

--- src/pages/寰球救援.ts
+++ src/pages/寰球救援.ts
@@ -1,12 +1,23 @@
 import { BasePage, Route } from './BasePage'
-import { createPageDetector, createRouteAction } from '../utils/img'
+import { createPageDetector, createRouteAction, tryCloseModals } from '../utils/img'
 
 export class 寰球救援 extends BasePage {
   name = '寰球救援'
   is = createPageDetector('images/寰球救援_1_0.9_418_2081_472_2107.png')
 
   免费(): boolean {
-    return createRouteAction('images/寰球救援$$免费_1_0.8_48_713_118_773.png')()
+    var action = createRouteAction('images/寰球救援$$免费_1_0.8_48_713_118_773.png')
+    for (var i = 0; i < 3; i++) {
+      if (action()) {
+        sleep(1000)
+        tryCloseModals()
+        sleep(500)
+        action()
+        return true
+      }
+      sleep(1000)
+    }
+    return false
   }
 
   routes(): Route[] {
