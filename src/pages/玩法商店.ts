b1aeb161f6f44269b84bf89167493fbff1036fe2
kming <1450030253@qq.com>
2026-07-13 00:01:21 +0800

    每日一刀零元购 fallback + 玩法商店检测图更新

--- src/pages/玩法商店.ts
+++ src/pages/玩法商店.ts
@@ -3,7 +3,7 @@ import { createPageDetector, createRouteAction } from '../utils/img'
 
 export class 玩法商店 extends BasePage {
   name = '玩法商店'
-  is = createPageDetector('images/玩法商店_1_0.9_442_485_639_538.png')
+  is = createPageDetector('images/玩法商店_1_0.9_108_1732_291_1779.png')
 
   back(): boolean {
     return createRouteAction('images/$关闭1_0_0.8_800_400_1020_600.png')()
