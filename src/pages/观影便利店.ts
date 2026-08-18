7a2735a4444b7200dfe6aedd0744fb2e60ddb808
kming <1450030253@qq.com>
2026-08-18 22:24:52 +0800

    活动:观影便利店领票优化,连领救援+远征券,已售罄识别区域调整

--- src/pages/观影便利店.ts
+++ src/pages/观影便利店.ts
@@ -6,23 +6,20 @@ export class 观影便利店 extends BasePage {
   is = createPageDetector('images/观影便利店_1_0.9_935_2272_1013_2323.png')
 
   免费(): boolean {
-    while (createTicketAction('images/观影便利店_救援入场券_0_0.9_106_500_230_2100.png', 'images/观影便利店_已售罄_0_0.9_842_500_940_2100.png')()) {
+    while (createTicketAction('images/观影便利店_救援入场券_0_0.9_106_500_230_2100.png', 'images/观影便利店_已售罄_0_0.9_837_500_957_2100.png')()) {
       var start = Date.now()
       while (Date.now() - start < 30000) {
         sleep(1000)
         var point = imageDetector('images/_恭喜获得_0_0.85_437_895_641_948.png')
-        log(point)
         if (point) {
           log('[观影便利店] 救援入场券门票领取成功(恭喜获得)')
           click(device.width / 2, device.height - 10)
-          sleep(300)
-          return true
+          sleep(800)
+          break
         }
       }
-      log('[观影便利店] 救援入场券门票 40 秒内未出现恭喜获得')
-      return false
     }
-    while (createTicketAction('images/观影便利店_远征入场券_0_0.9_109_500_226_2100.png', 'images/观影便利店_已售罄_0_0.9_842_500_940_2100.png')()) {
+    while (createTicketAction('images/观影便利店_远征入场券_0_0.9_109_500_226_2100.png', 'images/观影便利店_已售罄_0_0.9_837_500_957_2100.png')()) {
       var start = Date.now()
       while (Date.now() - start < 30000) {
         sleep(1000)
@@ -30,12 +27,10 @@ export class 观影便利店 extends BasePage {
         if (point) {
           log('[观影便利店] 远征入场券门票领取成功(恭喜获得)')
           click(device.width / 2, device.height - 10)
-          sleep(300)
-          return true
+          sleep(800)
+          break
         }
       }
-      log('[观影便利店] 远征入场券门票 40 秒内未出现恭喜获得')
-      return false
     }
     return true
   }
