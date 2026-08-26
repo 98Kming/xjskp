32ee27551024fc874fe18cdfc31106be805893b9
kming <1450030253@qq.com>
2026-08-26 12:45:34 +0800

    重构:补迁移军团商店.ts图片到IMG映射(计划遗漏)

--- src/pages/军团商店.ts
+++ src/pages/军团商店.ts
@@ -1,16 +1,22 @@
 import { BasePage, Route } from './BasePage'
 import { createMirroredAction, createPageDetector } from '../utils/img'
 import { 道具购买 } from './道具购买'
+import { sharedImages } from '../images'
+
+const IMG = {
+  ...sharedImages,
+  页面: 'images/军团商店_1_0.9_156_2016_254_2069.png',
+}
 
 export class 军团商店 extends BasePage {
   name = '军团商店'
-  is = createPageDetector('images/军团商店_1_0.9_156_2016_254_2069.png')
+  is = createPageDetector(IMG.页面)
 
   routes(): Route[] {
     return [
       // 两个入场券都点开道具购买弹窗，由道具购买的购买动作路由到对应玩法
-      { target: 道具购买, action: createMirroredAction('images/军团商店_环球救援入场券_1_0.9_115_1000_230_1118.png'), imagePath: 'images/军团商店_环球救援入场券_1_0.9_115_1000_230_1118.png' },
-      { target: 道具购买, action: createMirroredAction('images/军团商店_环球远征入场券_1_0.9_114_808_230_923.png'), imagePath: 'images/军团商店_环球远征入场券_1_0.9_114_808_230_923.png' },
+      { target: 道具购买, action: createMirroredAction(IMG.环球救援券), imagePath: IMG.环球救援券 },
+      { target: 道具购买, action: createMirroredAction(IMG.环球远征券), imagePath: IMG.环球远征券 },
     ]
   }
 }
